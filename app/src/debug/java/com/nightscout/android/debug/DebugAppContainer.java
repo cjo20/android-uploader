d48a3836ff630c6cfd14b7bf4e69b8e3e1275f35
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2014-12-21 11:25:35 -0800

    Debug drawer working.

--- app/src/debug/java/com/nightscout/android/debug/DebugAppContainer.java
+++ app/src/debug/java/com/nightscout/android/debug/DebugAppContainer.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.nightscout.android.R;
 import com.nightscout.android.ui.AppContainer;
@@ -15,6 +16,8 @@ import javax.inject.Singleton;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+
+import static butterknife.ButterKnife.findById;
 
 /**
  * An {@link AppContainer} for debug builds which wrap the content view with a sliding drawer on
@@ -34,12 +37,15 @@ public class DebugAppContainer implements AppContainer {
   }
 
   @InjectView(R.id.debug_drawer_layout) DrawerLayout drawerLayout;
+  @InjectView(R.id.debug_content) LinearLayout content;
 
 
   @Override
   public ViewGroup get(Activity activity) {
     this.activity = activity;
     drawerContext = activity;
+
+    activity.setContentView(R.layout.debug_activity_frame);
 
     // Manually find the debug drawer and inflate the drawer layout inside of it.
     ViewGroup drawer = findById(activity, R.id.debug_drawer);
@@ -49,6 +55,6 @@ public class DebugAppContainer implements AppContainer {
     ButterKnife.inject(this, activity);
 
 
-    return null;
+    return content;
   }
 }
--- app/src/debug/java/com/nightscout/android/debug/DebugNightscoutModule.java
+++ app/src/debug/java/com/nightscout/android/debug/DebugNightscoutModule.java
@@ -6,6 +6,7 @@ import com.nightscout.android.MainActivity;
 import com.nightscout.android.exceptions.Reporter;
 import com.nightscout.android.exceptions.StubbedReporter;
 import com.nightscout.android.modules.NightscoutModule;
+import com.nightscout.android.ui.AppContainer;
 
 import javax.inject.Singleton;
 
@@ -15,11 +16,15 @@ import dagger.Provides;
 @Module(
         addsTo = NightscoutModule.class,
         injects = {
-                MainActivity.class
+                MainActivity.class,
+                DebugAppContainer.class
         },
         overrides = true
 )
 public final class DebugNightscoutModule {
+    @Provides @Singleton AppContainer provideAppContainer(DebugAppContainer debugAppContainer) {
+        return debugAppContainer;
+    }
     @Provides @Singleton Reporter providesReporter(Application app) {
         return new StubbedReporter(app);
     }
